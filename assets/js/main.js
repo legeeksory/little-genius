da0bf60503d3ccc7b50a22acc175fe204ea6524c
ibrahimst <43015077+legeeksory@users.noreply.github.com>
2021-06-17 01:35:05 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,24 @@ $('.menu-toggle').click(function() {
     $(this).toggleClass('nav');
        
   })
+
+  //#################### DETECTER LE SCROLL TOP ######################
+var scrollTopBtn = document.getElementById("scroll_to_top"); 
+var rootElement = document.documentElement;
+
+//Ajout d'un évenement d'écoute de click au bouton
+function handleScroll() {
+    // Nombre total de pixel que nous pouvons faire défiler
+    var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+    if ((rootElement.scrollTop / scrollTotal ) > 0.20 ) {
+        // Show button
+        scrollTopBtn.classList.add("show-scroll")
+      } else {
+        // Hide button
+        scrollTopBtn.classList.remove("show-scroll");
+      }
+}
+
+document.addEventListener("scroll", handleScroll);
+
+//#################### FIN DETECTER LE SCROLL TOP ######################
